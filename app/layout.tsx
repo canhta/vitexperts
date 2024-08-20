2ac1b193833cedfa0f23a9fe3b97958e7aa29aa2
CanhTa <canh.tc98@gmail.com>
2024-08-20 16:35:58 +0700

    feat: vercel speed check

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
-"use client";
-import { useEffect } from "react";
-import "./globals.css";
+'use client';
+import { useEffect } from 'react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import './globals.css';
 
 type Props = {
   children: React.ReactNode;
@@ -8,15 +9,18 @@ type Props = {
 
 export default function RootLayout({ children }: Props) {
   useEffect(() => {
-    require("bootstrap/dist/js/bootstrap.min.js");
+    require('bootstrap/dist/js/bootstrap.min.js');
   }, []);
 
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <link rel="icon" type="image/png" href="../favicon.ico" />
+        <link rel='icon' type='image/png' href='../favicon.ico' />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
